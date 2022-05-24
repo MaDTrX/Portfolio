5554e082d96b3526ba0aca7af36bba55e6751173
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-05-24 15:14:51 -0700

    design update

---
+++ public/javascripts/script.js
@@ -0,0 +1,44 @@
+const sliders = document.querySelectorAll('.slide')
+const mobile = document.getElementById('mobile')
+const desktop = document.getElementById('desktop')
+const demos= document.querySelectorAll('.image')
+const previews = document.querySelectorAll('.preview')
+const infos = document.querySelectorAll('.info')
+const appearOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -250px 0px'
+}
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return
+        } else {
+
+            entry.target.classList.add('appear')
+            appearOnScroll.unobserve(entry.target)
+           
+        }
+    })
+}, appearOptions)
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider)
+})
+mobile.addEventListener('click', () => {
+        console.log('hi')
+        previews.forEach(preview => {
+            preview.style.display = 'none';
+        })
+        infos.forEach(info => {
+            info.style.display = 'none';
+        })
+        demos.forEach(demo => {
+            demo.style.display = 'flex';
+        })
+    
+})
+desktop.addEventListener('click', () => {
+    console.log('hi')
+   
+})
