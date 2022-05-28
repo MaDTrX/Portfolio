8ed7912ea73aa997bc7ce5198263c0720eaed45d
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-05-28 15:13:31 -0700

    Trim

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Marlin Makori | Full Stack Developer' });
-});
-
-module.exports = router;
