0364894bd2d134a9f5103486c576618fb902f4a2
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-06-01 18:34:46 -0700

    nav bar res

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const desktop = document.getElementById('desktop')
 const demos= document.querySelectorAll('.image')
 const previews = document.querySelectorAll('.preview')
 const infos = document.querySelectorAll('.info')
+const hamburger = document.getElementById('open')
+const close = document.getElementById('close')
+const mobileNav = document.getElementById('mobile-nav')
+const links = document.querySelectorAll('a')
 const appearOptions = {
     threshold: 0,
     rootMargin: '0px 0px -250px 0px'
@@ -25,20 +29,37 @@ const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
 sliders.forEach(slider => {
     appearOnScroll.observe(slider)
 })
-mobile.addEventListener('click', () => {
-        console.log('hi')
-        previews.forEach(preview => {
-            preview.style.display = 'none';
-        })
-        infos.forEach(info => {
-            info.style.display = 'none';
-        })
-        demos.forEach(demo => {
-            demo.style.display = 'flex';
-        })
+// mobile.addEventListener('click', () => {
+//         console.log('hi')
+//         previews.forEach(preview => {
+//             preview.style.display = 'none';
+//         })
+//         infos.forEach(info => {
+//             info.style.display = 'none';
+//         })
+//         demos.forEach(demo => {
+//             demo.style.display = 'flex';
+//         })
     
-})
-desktop.addEventListener('click', () => {
+// })
+hamburger.addEventListener('click', () => {
+    close.style.display = 'flex'
+    mobileNav.style.display = 'flex'
+    hamburger.style.display = 'none'
+    mobileNav.classList.add('responsive')
     console.log('hi')
-   
 })
+close.addEventListener('click', () => {
+    mobileNav.style.display = 'none'
+    close.style.display = 'none'
+    hamburger.style.display = 'flex'
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileNav.style.display = 'none'
+        close.style.display = 'none'
+        hamburger.style.display = 'flex'
+    })
+})
+
